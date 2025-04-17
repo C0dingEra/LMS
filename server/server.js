aa25097a69673dfa59d8c39f6597e3d409d92569
Evade Won <akashsingh926369@gmail.com>
2025-04-17 16:58:24 +0530

    added model or schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDb from './database/dbConnect.js';
-dotenv.config();
+dotenv.config({});
 const app=express();
 
-const port=process.env.PORT || 5000;
+const port=process.env.PORT;
 app.listen(port,(req,res)=>{
     console.log(`server is running at ${port}`)
 })
